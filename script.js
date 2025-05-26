e267a501133c7c2520c7c7b68d5d088c3074751e
Beridze0 <beridzedavit904@gmail.com>
2025-05-26 17:34:46 +0400

    slider part 1

--- script.js
+++ script.js
@@ -179,12 +179,10 @@ const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImage = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
   // Replace src with data-src
-  console.log(entry.target.src);
 
   entry.target.src = entry.target.dataset.src;
 
@@ -198,9 +196,47 @@ const loadImage = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImage, {
   root: null,
   threshold: 0,
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`)
+  );
+};
+
+goToSlide(0);
+
+//Next Slide
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else curSlide++;
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide = 1;
+  } else curSlide--;
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+//-100% 0% 100% 200%
 
 // const h1 = document.querySelector('h1');
 
